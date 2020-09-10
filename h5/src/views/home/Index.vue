e52785cb1c7363baadafcb69c446d99600f72d9a
Administrator <Administrator@SC-201904020931>
2020-09-10 08:35:28 +0800

    提交

--- h5/src/views/home/Index.vue
+++ h5/src/views/home/Index.vue
@@ -535,7 +535,11 @@ export default {
    
      // 获取门店列表数据
     getList: function() {
-       alert("欢迎你");
+        getNearStoreData().then(res => {
+           this.$set(this, "storeList", res.data.storeList);
+           this.$set(this, "nearGoodList", res.data.nearGoodList);
+          });
+    /*
        if (!cookie.get(LATITUDE) && !cookie.get(LONGITUDE)){
           getNearStoreData().then(res => {
            this.$set(this, "storeList", res.data.storeList);
@@ -559,6 +563,7 @@ export default {
                this.$dialog.error(err.msg);
            });
 	  }
+	  */
     },
     closeFollow() {
       this.followHid = false;
