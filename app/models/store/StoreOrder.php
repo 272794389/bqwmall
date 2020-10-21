4206cf39d53e39d4366ef6b2d8636d601ad37740
XinChow <XinChow@LAPTOP-Q54K8LTG>
2020-10-21 23:21:14 +0800

    1

--- app/models/store/StoreOrder.php
+++ app/models/store/StoreOrder.php
@@ -888,7 +888,7 @@ class StoreOrder extends BaseModel
      */
     public static function BackPoint($order)
     {
-        return Db::name('user')->where('uid',$order['uid'])->dec('pay_point',$order['pay_point'])->update();
+        return Db::table('eb_user')->where('uid',$order['uid'])->dec('pay_point',$order['pay_point'])->update();
         //return Db::name('user')->getLastSql();
     }
 
