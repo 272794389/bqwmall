f6582a90f929b0d71dc25adbbd9452e9903e127b
XinChow <XinChow@LAPTOP-Q54K8LTG>
2020-10-21 22:49:53 +0800

    1

--- app/api/controller/admin/StoreOrderController.php
+++ app/api/controller/admin/StoreOrderController.php
@@ -604,7 +604,7 @@ class StoreOrderController
         //回退积分
         //StoreOrder::BackPoint($orderInfo);
         //User::where('uid',$orderInfo['uid'])->dec('pay_point',$orderInfo['pay_point'])->update();
-        Db::table("user")->where('uid',$orderInfo['uid'])->dec('pay_point',$orderInfo['pay_point'])->update();
+        Db::table("eb_user")->where('uid',$orderInfo['uid'])->dec('pay_point',$orderInfo['pay_point'])->update();
         ///return (StoreOrder::getLastSql());
         $sqlxx = StoreOrder::getLastSql();
         $resEdit = StoreOrder::edit($data, $orderInfo['id'], 'id');
